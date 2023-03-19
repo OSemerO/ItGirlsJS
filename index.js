f07578f329f7f20be523819942626871aff09f12
O_Semernina <semernina-olga@mail.ru>
2023-03-19 21:04:31 +0300

    week 9, JS the first lesson

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 'use strict'
-function ask() {
-    let name = prompt("Как тебя зовут?","");
-    alert(`Привет, ${name}!`);
-};
+let cleanerSalary = 40000;
+let singerSalary = 22000;
+let cleanerSingSalary = cleanerSalary + singerSalary;
+
+console.log(cleanerSingSalary);
+
+let city = "Владивосток";
+let num = 2000;
+let song = city + " " + num;
+
+console.log(song);
+
+let messageTime = 128;
+let likeTime = 254;
+let vkTime = 137;
+let youtubeTime = 201;
+let rusalkaTime = messageTime + likeTime + vkTime + youtubeTime;
+
+console.log(rusalkaTime);
+
+let x = 2;
+let sum = x**5;
+
+console.log(sum);
+
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+const greetings = "Привет";
+const name = "Кот";
+
+alert(greetings + ", " + name);
+
+let time = "";
+ time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+ time = 19;
+ console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+ time = 9;
+ console.log( 'Немного не новостей, но событий. Осталось минут: ' + time);
+ time = 0;
+ console.log('Осталось минут ' + time + '.' + ' Вы приехали. Добро пожаловать в Москву!');
+
+ let fahrenheit = 451;
+ let celsius = (fahrenheit - 32) / 1.8;
+
+ console.log(fahrenheit + ' градус по Фаренгейту - это ' + celsius + ' градуса по Цельсию.');
 
  
- let op; 
-
- function calc() {
-   
-   let result;
-  
-   let num1 = Number(document.getElementById("num1").value);
-   let num2 = Number(document.getElementById("num2").value);
- 
-   switch (op) {
-     case '+':
-       result = num1 + num2;
-       break;
-     case '-':
-       result = num1 - num2;
-       break;
-     case '*':
-       result = num1 * num2;
-       break;
-     case '/':
-       result = num1 / num2;
-       break;
-   }
-
-   document.getElementById("result").innerHTML = result;
- };
