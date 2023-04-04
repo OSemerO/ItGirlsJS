5cd030c3e8cf45a9c06c4a10eef300c61f9e0510
O_Semernina <semernina-olga@mail.ru>
2023-04-04 21:46:09 +0300

    DOM and BOM, some practice

--- index.js
+++ index.js
@@ -3,5 +3,12 @@
 // Найди карточку Стёпы в коде и запиши пол Стёпы в переменную 
 // Измени пол Стёпы на мужской используя метод textContent
 
-let Stepa = document.querySelectorAll('card');
-console.log(Stepa);
+// пока не сделала
+
+// console.log(location.href); // показывает текущий URL
+// if (confirm("Перейти на Wikipedia?")) {
+// 	// перенаправляет браузер на другой адрес
+//   location.href = "https://wikipedia.org"; 
+// }
+
+
