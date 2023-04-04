35daf64c69f7b68a8ad4f1bd6a6ac89ecf75fd49
O_Semernina <semernina-olga@mail.ru>
2023-04-04 21:13:45 +0300

    practicing querySelector

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 'use strict'
 
-function showMe () {
-console.log("Я учу JavaScript!");
-}
+// Найди карточку Стёпы в коде и запиши пол Стёпы в переменную 
+// Измени пол Стёпы на мужской используя метод textContent
 
-showMe();
-
-const show = () => console.log("Я учу JavaScript!");
-
-show();
-
-const blockFoto = document.getElementsByClassName("image") [0];
-
-const nextShow = () => {
-  blockFoto.style.backgroundImage = "url('https://old.bigenc.ru/media/2016/10/27/1235153355/3761.jpg')";
-  console.log(blockFoto);
-};
-
-const backShow = () => {
-  blockFoto.style.backgroundImage = "url('https://sun9-76.userapi.com/impg/c_XdhklPf_RfWuTQr1I1wS8v7pSKkpW4hOMDwQ/UV3H6kXmZIY.jpg?size=807x534&quality=96&sign=138f5aca1360f4b1f3bc82a758a3e8cc&type=album')";
-  console.log(blockFoto);
-};
-
-
-let year = prompt("Какой сейчас год?");
-if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)){
-    console.log('Високосный');
-} else {
-    console.log('Не високосный');
-};
+let Stepa = document.querySelectorAll('card');
+console.log(Stepa);
