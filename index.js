20469260f7d8da6185118baf191310ccf1d5ffdc
O_Semernina <semernina-olga@mail.ru>
2023-04-15 21:02:30 +0300

    doing homework week 12, comments, with checking upper/lower case

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
+const urlElement = document.getElementById("url");
+const textElement = document.getElementById("text");
+const commentElement = document.getElementById("comment");
+let comment = commentElement.textContent;
 
+const buttonElement = document.querySelector(".send");
+const blockElement = document.querySelector(".comments__block");
+const arr = [
+  "https://disney-classic.ru/wp-content/uploads/2022-06-14_15-12-53-300x290.png",
+  "https://upload.wikimedia.org/wikipedia/ru/thumb/f/fe/James_P_Sullivan.jpg/280px-James_P_Sullivan.jpg",
+  "https://s0.rbk.ru/v6_top_pics/media/img/1/83/756079611261831.jpg",
+  "https://img.rl0.ru/afisha/-x-q85i/s2.afisha.ru/mediastorage/39/15/4d6179bcd85c4ea5a7c564111539.jpg",
+  "https://kartinkof.club/uploads/posts/2022-04/1649784841_1-kartinkof-club-p-rzhachnie-kartinki-osel-iz-shreka-1.jpg",
+];
 
-let totalPriceElement = document.getElementById('total');
-let price = totalPriceElement.textContent; 
-console.log(price);
-const discount = document.querySelector(".discount"); //кнопка
+buttonElement.addEventListener("click", () => {
+  let url = "";
+  console.log(!urlElement.value);
+  if (!urlElement.value) {
+    const indexArr = Math.floor(Math.random() * 5);
+    console.log();
+    url = arr[indexArr];
+  } else {
+    url = urlElement.value;
+  };
 
-discount.addEventListener("click", () => {
-    totalPriceElement.textContent = +price - (+price * 15 / 100);});
+  const text = textElement.value;
+  const arrFio = text.split(" ");
 
+  function checkedWord(word) {
+    return `${word.slice(0, 1).toUpperCase()}${word.slice(1).toLowerCase()} `;
+  };
+  
+  let surname = checkedWord(arrFio[0]);
+  let nameUser = checkedWord(arrFio[1]);
+  let patronim = checkedWord(arrFio[2]);
+  console.log(surname);
 
+  const totalString = `${surname} ${nameUser} ${patronim}`;
 
-
-
-
+  blockElement.innerHTML = `
+      <div class="border">
+        <img
+          src=${url}
+          alt="картинка"
+          class="imgUserAvatar"
+        />
+        <p class="text">${totalString}</p>
+        <p class="text>${comment}</p>
+      </div>
+    `;
+});
 
