4a95d9acb5c05bf274b2a4e104d07058c6205b13
O_Semernina <semernina-olga@mail.ru>
2023-05-08 14:49:14 +0300

    homework 13, doing chat with date

--- index.js
+++ index.js
@@ -1,53 +1,73 @@
-const urlElement = document.getElementById("url");
-const textElement = document.getElementById("text");
-const commentElement = document.getElementById("comment");
+// переменные для получения данных, что ввел пользователь
+
+let nameElement = document.getElementById("name_input").value;
+let urlElement = document.getElementById("url_input").value;
+let commentElement = document.getElementById("message").value;
 let comment = commentElement.textContent;
 
-const buttonElement = document.querySelector(".send");
-const blockElement = document.querySelector(".comments__block");
+// находим кнопку
+const buttonElement = document.querySelector(".button");
+let chatElement = document.querySelector(".chat");
+
+
+// массив с аватарками для пользователя
 const arr = [
   "https://disney-classic.ru/wp-content/uploads/2022-06-14_15-12-53-300x290.png",
   "https://upload.wikimedia.org/wikipedia/ru/thumb/f/fe/James_P_Sullivan.jpg/280px-James_P_Sullivan.jpg",
   "https://s0.rbk.ru/v6_top_pics/media/img/1/83/756079611261831.jpg",
   "https://img.rl0.ru/afisha/-x-q85i/s2.afisha.ru/mediastorage/39/15/4d6179bcd85c4ea5a7c564111539.jpg",
   "https://kartinkof.club/uploads/posts/2022-04/1649784841_1-kartinkof-club-p-rzhachnie-kartinki-osel-iz-shreka-1.jpg",
+  "https://cs13.pikabu.ru/post_img/2023/01/30/9/16750925851227285.jpg",
 ];
+
+// навешиваем обработчик события на кнопку, с условием, что если поле URL пустое, произошла рандомная подстановка аватарки
 
 buttonElement.addEventListener("click", () => {
   let url = "";
   console.log(!urlElement.value);
   if (!urlElement.value) {
-    const indexArr = Math.floor(Math.random() * 5);
+    const indexArr = Math.floor(Math.random() * 6);
     console.log();
     url = arr[indexArr];
   } else {
     url = urlElement.value;
-  };
-
-  const text = textElement.value;
-  const arrFio = text.split(" ");
-
-  function checkedWord(word) {
-    return `${word.slice(0, 1).toUpperCase()}${word.slice(1).toLowerCase()}`;
-  };
-  
-  let surname = checkedWord(arrFio[0]);
-  let nameUser = checkedWord(arrFio[1]);
-  let patronim = checkedWord(arrFio[2]);
-  console.log(surname);
-
-  const totalString = `${surname} ${nameUser} ${patronim}`;
-
-  blockElement.innerHTML = `
-      <div class="border">
-        <img
-          src=${url}
-          alt="картинка"
-          class="imgUserAvatar"
-        />
-        <p class="text">${totalString}</p>
-        <p class="text>${comment}</p>
-      </div>
-    `;
+  }
 });
+
+// работаем с данными, что ввел пользователь - делаем массив 
+let userNameArr = nameElement.split(' ');   
+
+// функция на проверку верхних/нижних регистров
+function checkedWord(word) {
+  return `${word.slice(0, 1).toUpperCase()} ${word.slice(1).toLowerCase()}`;
+};
+
+// заводим новые переменные для вытащенных из массива данных пользователя и приведенных к регистру
+
+let surname = checkedWord(userNameArr[0]);
+let nameUser = checkedWord(userNameArr[1]);
+let patronim = checkedWord(userNameArr[2]);
+
+// собираем общее ФИО
+
+const totalName = `${surname} ${nameUser} ${patronim}`;
+    
+ // проверка на спам
+let commentChecked = () => {
+  let checkSpam = comment.replace(/viagra/gi, "цензура").replace(/xxx/gi, "цензура");
+  return checkSpam;
+};
+
+// let re = /яблоки/gi;
+// let str = 'Яблоки круглые и яблоки сочные.';
+// let newstr = str.replace(re, 'апельсины');
+// console.log(newstr); // апельсины круглые и апельсины сочные.
+
+// собираем все элементы для чата 
+
+document.querySelector('.username').innerText = `<div class="username">${checkedWord()}</div>`;
+
+document.querySelector('.message').innerHTML = `<div class="message__wrap">${commentChecked()}</div>`;  
+
+document.querySelector('.avatar').innerHTML = `<div class="avatar" img src="${avatar}">`; 
 
